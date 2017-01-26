886f7cdb09047971446033130045f1b15b56ca57
HaoGuangBo <haoguangbo@yeah.net>
2017-01-26 18:07:51 +0800

    ipc test successfully

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
 
+// import { ipc } from '../ipc';
+const {ipcRenderer} = require('electron');
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+
+
+
+
 @Component({
   selector: 'my-app',
   templateUrl:'app/app.component.html'
   // template: ``,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  test(){
+    // console.log(ipc);
+    ipcRenderer.once('asynchronous-reply', (event:any, arg:any) => {
+      console.log(arg); // prints "pong"
+    });
+    ipcRenderer.send('asynchronous-message', 'ping');
+  };
+
+}
